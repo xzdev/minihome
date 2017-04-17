11d7182c0390fc2618736169f225a82301c6d835
xzdev <xiaoyuzh@gmail.com>
2017-04-17 00:21:47 -0400

    enable blog editor to preview markdown

--- www/src/app/components/BlogEditor/index.js
+++ www/src/app/components/BlogEditor/index.js
@@ -1,41 +1,68 @@
 import React, { Component, PropTypes } from 'react';
-
-const renderPreview = function renderPreview() {
-  return (
-    <div>preview</div>
-  );
-};
-
-const renderEditor = function renderEditor() {
-  return (
-    <div>
-      <div>
-        <div>Title:</div>
-        <div><input /></div>
-      </div>
-      <div>
-        <textarea />
-      </div>
-    </div>
-  );
-};
+import ReactMarkdown from 'react-markdown';
 
 class BlogEditor extends Component {
   static propTypes = {
-    content: PropTypes.object,
+    blog: PropTypes.object,
     onSubmit: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
-    content: {},
+    blog: {
+      title: '',
+      content: '',
+    },
   }
 
   constructor(props) {
     super(props);
+    const blog = props.blog;
+
     this.state = {
       isPreview: false,
-      content: props.content,
+      title: blog.title,
+      blog: blog.blog,
     };
+
+    this.updateTitle = this.updateTitle.bind(this);
+    this.updateContent = this.updateContent.bind(this);
+  }
+
+  updateContent(evt) {
+    this.setState({
+      content: evt.target.value,
+    });
+  }
+
+  updateTitle(evt) {
+    this.setState({
+      title: evt.target.value,
+    });
+  }
+
+  renderPreview() {
+    return (
+      <div>
+        <div>{this.state.title}</div>
+        <ReactMarkdown source={this.state.content} />
+      </div>
+    );
+  }
+
+  renderEditor() {
+    return (
+      <div>
+        <div>
+          <div>Title:</div>
+          <div>
+            <input onChange={this.updateTitle} value={this.state.title} />
+          </div>
+        </div>
+        <div>
+          <textarea onChange={this.updateContent} value={this.state.content} />
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -43,8 +70,8 @@ class BlogEditor extends Component {
       <div>
         <div>
           {
-            this.state.isPreview ? renderPreview(this.state.content)
-            : renderEditor(this.state.content)
+            this.state.isPreview ? this.renderPreview()
+            : this.renderEditor(this.state.content)
           }
         </div>
         <div>
